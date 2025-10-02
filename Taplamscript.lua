291e6099686bf482a9dee0a7efdc38c523c58d92
PHONG520 <nhuphong06062019@gmail.com>
2025-10-02 11:23:07 +0700

    Add files via upload

---
+++ Taplamscript.lua
@@ -0,0 +1,110 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
+
+       local Window = MakeWindow({
+         Hub = {
+         Title = "Nguyễn Như Phong",
+         Animation = "ID Tiktok:nguyen.nhu.phong.123"
+         },
+        Key = {
+        KeySystem = false,
+        Title = "Key System",
+        Description = "",
+        KeyLink = "",
+        Keys = {"1234"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "Running the Script...",
+       Incorrectkey = "The key is incorrect",
+       CopyKeyLink = "Copied to Clipboard"
+      }
+    }
+  })
+
+       MinimizeButton({
+       Image = "http://www.roblox.com/asset/?id=88176015855368",
+       Size = {40, 40},
+       Color = Color3.fromRGB(10, 10, 10),
+       Corner = true,
+       Stroke = false,
+       StrokeColor = Color3.fromRGB(255, 0, 0)
+      })
+      
+------ Tab
+    local Tab1o = MakeTab({Name = "Script Farm"})
+     
+------- BUTTON
+    
+    AddButton(Tab1o, {
+     Name = "Redz Hub beta",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+getgenv().BETA_VERSION = true
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tlredz/Scripts/refs/heads/main/main.luau"))(Settings)
+  end
+  })
+
+    AddButton(Tab1o, {
+     Name = "Hiru Hub",
+    Callback = function()
+	  getgenv().Settings = {
+    JoinTeam = true,
+    Team = "Marines"
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kiddohiru/Source/main/BloxFruits.lua"))()
+  end
+  })
+
+    AddButton(Tab1o, {
+     Name = "banana hub fake",
+    Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PHONG520/obiiyeuem/refs/heads/main/BananaHub.txt"))()
+  end
+  })
+    AddButton(Tab1o, {
+     Name = "zee hub",
+    Callback = function()
+loadstring(game:HttpGet("https://link.trwxz.com/LS-Zee-Hub-VIP"))()
+  end 
+  })
+
+    AddButton(Tab1o, {
+     Name = "blue x hub",
+    Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/BlueX-Hub/refs/heads/main/Main.lua"))()
+  end
+  })
+
+    AddButton(Tab1o, {
+     Name = "teddy hub",
+    Callback = function()
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Teddyseetink/Haidepzai/refs/heads/main/TeddyHub.lua"))()
+  end
+  })
+
+    AddButton(Tab1o, {
+     Name = "hoho hub",
+    Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI"))()
+  end
+  })
+
+    AddButton(Tab1o, {
+     Name = "Alchemy Hub",
+    Callback = function()
+loadstring(game:HttpGet("https://scripts.alchemyhub.xyz"))()
+  end
+  })
+
+    AddButton(Tab1o, {
+     Name = "Quantum hub",
+    Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/flazhy/QuantumOnyx/refs/heads/main/QuantumOnyx.lua"))()
+  end
+  })
+
+
